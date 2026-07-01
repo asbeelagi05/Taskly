80a066d9551bdef6647ffe4a0ec3b0026fbefd5e
asbeelagi05 <beelagiabhishek@gmial.com>
2026-07-01 17:01:50 +0530

    Complete customer module with modern Bootstrap UI

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from config import Config
-from extensions import db
-from routes.auth import auth
+
 from extensions import db, login_manager
+
+from routes.auth import auth
 from routes.customer import customer
 from routes.labour import labour
-from models.job import Job
 from routes.jobs import jobs
+
+from models.user import User
+from models.job import Job
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
@@ -17,14 +21,15 @@ app.register_blueprint(auth)
 app.register_blueprint(customer)
 app.register_blueprint(labour)
 app.register_blueprint(jobs)
-from models.user import User
 
 with app.app_context():
     db.create_all()
 
+
 @app.route("/")
 def home():
     return "<h1>Taskly Backend Running</h1>"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
